6615d06f507b392103d91d4bf7df8bb9a8c6575c
Student <example@example.com>
2019-11-12 16:41:29 -0800

    add broken code that needs fixed

--- src/journ.js
+++ src/journ.js
@@ -1,1 +1,18 @@
+function Journal (title, secrets){
+  this.array = [],
+  this.wordTotal = 0,
+  this.title = title,
+  this.secrets = secrets
+}
 
+Journal.prototype.countLetters = function(entry) {
+  this.split();
+  for (var i = 0; i <= this.array.length; i++) {
+    this.wordTotal += 1
+  }
+  return this.wordTotal
+}
+
+export function theJournal() {
+
+}
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
+import { theJournal } from './journ';
 import './styles.css';
 
 $(document).ready(function() {
   $("form").submit(function(event){
     event.preventDefault();
-    $("#finalOutput").text("Hey");
+    const newArray = [];
+    const title = $("#titleInput").val();
+    const secrets = $("#diaryInput").val();
+    newArray.push(secrets);
+    console.log(title, secrets, newArray);
+    $("#finalOutput").text(title + "\n" + secrets);
   });
 });
